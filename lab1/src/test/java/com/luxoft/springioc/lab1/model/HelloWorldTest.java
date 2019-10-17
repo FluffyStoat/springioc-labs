2a2d2ec8c4a21b26273847949e2772bb284aabe3
Konstantin Bochkarev <bkv.developer@gmail.com>
2019-10-17 10:54:29 +0400

    add labs 2-3

--- lab1/src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
+++ lab1/src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-	"classpath:application-context.xml",
+    "classpath:application-context.xml",
 })
 public class HelloWorldTest {
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "classpath:application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "classpath:application-context.xml";
 /*
 	//Get context from test scope.
 	@Autowired
 	private ApplicationContext context;
 */
 
-	@Autowired
-	private Person russianPerson;
-	@Autowired
-	private Person russianPerson2;
-	@Autowired
-	private Person americanPerson;
+    @Autowired
+    private Person russianPerson;
+    @Autowired
+    private Person russianPerson2;
+    @Autowired
+    private Person americanPerson;
     @Resource(name = "canadian")
     private Person canadianPerson;
     @Autowired
@@ -42,103 +42,103 @@ public class HelloWorldTest {
     @Autowired
     private Room test;
     @Autowired
-	private Car car;
-	@Autowired
-	private Port port;
+    private Car car;
+    @Autowired
+    private Port port;
 
-	private Person expectedRussianPerson;
-	private Person expectedAmericanPerson;
+    private Person expectedRussianPerson;
+    private Person expectedAmericanPerson;
 
     private AbstractApplicationContext context;
 
-	@Before
-	public void setUp() {
-		context = new FileSystemXmlApplicationContext(new String[] { APPLICATION_CONTEXT_XML_FILE_NAME });
-		expectedRussianPerson = getRussianPerson();
-		expectedAmericanPerson = getAmericanPerson();
-	}
+    @Before
+    public void setUp() {
+        context = new FileSystemXmlApplicationContext(new String[]{APPLICATION_CONTEXT_XML_FILE_NAME});
+        expectedRussianPerson = getRussianPerson();
+        expectedAmericanPerson = getAmericanPerson();
+    }
 
-	@Test
-	public void testInitPerson() {
-		UsualPerson russian = (UsualPerson) context.getBean("russianPerson", Person.class);
-		UsualPerson american = (UsualPerson) context.getBean("americanPerson", Person.class);
+    @Test
+    public void testInitPerson() {
+        UsualPerson russian = (UsualPerson) context.getBean("russianPerson", Person.class);
+        UsualPerson american = (UsualPerson) context.getBean("americanPerson", Person.class);
         Group group = context.getBean(Group.class);
         Room room = group.getRoom();
 
-		assertEquals(expectedRussianPerson, russian);
-		assertEquals(expectedAmericanPerson, american);
+        assertEquals(expectedRussianPerson, russian);
+        assertEquals(expectedAmericanPerson, american);
 
-		assertEquals(expectedRussianPerson, russianPerson);
-		assertEquals(expectedAmericanPerson, americanPerson);
+        assertEquals(expectedRussianPerson, russianPerson);
+        assertEquals(expectedAmericanPerson, americanPerson);
 
-		assertNotEquals(russianPerson, americanPerson);
+        assertNotEquals(russianPerson, americanPerson);
 
-		assertEquals(101, room.getNumber());
-		assertEquals("15", room.getProperties().getProperty("quantity"));
-		assertEquals("true", room.getProperties().getProperty("isNetworkPresent"));
-		assertNotNull(resourceGroup);
+        assertEquals(101, room.getNumber());
+        assertEquals("15", room.getProperties().getProperty("quantity"));
+        assertEquals("true", room.getProperties().getProperty("isNetworkPresent"));
+        assertNotNull(resourceGroup);
 
-		assertNotNull(russianPerson2);
+        assertNotNull(russianPerson2);
 
-		System.out.println("Russian BeanFactory:\n" + russian);
-		System.out.println("American BeanFactory:\n" + american);
+        System.out.println("Russian BeanFactory:\n" + russian);
+        System.out.println("American BeanFactory:\n" + american);
 
-		System.out.println("Russian Autowired:\n" + russianPerson);
-		System.out.println("Russian Autowired2:\n" + russianPerson2);
+        System.out.println("Russian Autowired:\n" + russianPerson);
+        System.out.println("Russian Autowired2:\n" + russianPerson2);
 
-		System.out.println("American Autowired:\n" + americanPerson);
+        System.out.println("American Autowired:\n" + americanPerson);
         System.out.println("Canadian Resource:\n" + canadianPerson);
         System.out.println("Default:\n" + defaultPerson);
 
         System.out.println("Group:\n" + group);
         System.out.println("Resource group:\n" + resourceGroup);
 
-		System.out.println("Room:\n" + test);
+        System.out.println("Room:\n" + test);
 
-		System.out.println("Car Engine 1:\n" + car.getFirstEngine());
-		System.out.println("Car Engine 2:\n" + car.getSecondEngine());
+        System.out.println("Car Engine 1:\n" + car.getFirstEngine());
+        System.out.println("Car Engine 2:\n" + car.getSecondEngine());
 
-		System.out.println("Port:\n" + port);
-	}
+        System.out.println("Port:\n" + port);
+    }
 
-	private UsualPerson getRussianPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(35);
-		person.setHeight(175);
-		person.setIsProgrammer(true);
-		person.setName("Peter Petrov");
+    private UsualPerson getRussianPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(35);
+        person.setHeight(175);
+        person.setIsProgrammer(true);
+        person.setName("Peter Petrov");
 
-		Country country = new Country();
-		country.setId(1);
-		country.setName("Russia");
-		country.setCodeName("RU");
+        Country country = new Country();
+        country.setId(1);
+        country.setName("Russia");
+        country.setCodeName("RU");
 
-		person.setCountry(country);
+        person.setCountry(country);
 
-		return person;
-	}
+        return person;
+    }
 
-	private Person getAmericanPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(29);
-		person.setHeight(173);
-		person.setIsProgrammer(true);
-		person.setName("John Smith");
+    private Person getAmericanPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(29);
+        person.setHeight(173);
+        person.setIsProgrammer(true);
+        person.setName("John Smith");
 
-		Country country = new Country();
-		country.setId(2);
-		country.setName("Usa");
-		country.setCodeName("US");
+        Country country = new Country();
+        country.setId(2);
+        country.setName("Usa");
+        country.setCodeName("US");
 
-		person.setCountry(country);
+        person.setCountry(country);
 
-		return person;
-	}
+        return person;
+    }
 
-	@After
-	public void tearDown() {
-		if (context != null) {
-			context.close();
-		}
-	}
+    @After
+    public void tearDown() {
+        if (context != null) {
+            context.close();
+        }
+    }
 }
